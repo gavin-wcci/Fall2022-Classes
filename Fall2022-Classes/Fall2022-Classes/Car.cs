d5ae87405cbd6473bad09f7b91ffbddc98e36995
Gavin <gavin@wecancodeit.org>
2022-09-13 10:53:04 -0400

    created car class with properties

---
+++ Fall2022-Classes/Fall2022-Classes/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2022_Classes
+{
+    public class Car
+    {
+        //properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Speed { get; set; }
+        public bool IsPaidFor { get; set; }
+
+
+        //methods
+    }
+}
