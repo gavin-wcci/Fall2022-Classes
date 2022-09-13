96a3aa75ced7b22069a6bdc0e3ef22bba1907e04
Gavin <gavin@wecancodeit.org>
2022-09-13 11:57:36 -0400

    added constructors to car class

--- Fall2022-Classes/Fall2022-Classes/Car.cs
+++ Fall2022-Classes/Fall2022-Classes/Car.cs
@@ -16,7 +16,39 @@ namespace Fall2022_Classes
         public int Speed { get; set; }
         public bool IsPaidFor { get; set; }
 
+        //access modifiers: public vs. private vs. internal
+        //  How we can access classes, properties, methods, etc. in our application
+
+        //constructor
+        public Car()
+        {
+            Make = "Lexus";
+            Model = "RX 350";
+            Color = "White";
+            Year = 2018;
+            Speed = 0;
+            IsPaidFor = false;
+        }
+
+        public Car(string make, string model, string color, int year)
+        {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year; 
+        }
+
+        public Car(string make, string model, string color, int year, int speed, bool isPaidFor)
+        {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+            Speed = speed;
+            IsPaidFor = isPaidFor;
+        }
 
         //methods
+
     }
 }
--- Fall2022-Classes/Fall2022-Classes/Program.cs
+++ Fall2022-Classes/Fall2022-Classes/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using Fall2022_Classes;
+
+//creating a car with our default constructor
+Car ashleysCar = new Car();
+
+Console.WriteLine("Ashley has a " + ashleysCar.Make);
+
+ashleysCar.IsPaidFor = true;
+
+Console.WriteLine("This car is paid for? : " + ashleysCar.IsPaidFor);
+
+var gavCarMake = "Toyota";
+
+//creating another car with a different constructor
+Car gavinsCar = new Car(gavCarMake, "Camry", "Grey", 2007);
+
+Console.WriteLine("Gavin has a " + gavinsCar.Make);
+
+
+//making a list of objects from out new class
+var garage = new List<Car>();
+
+//adding our cars to the list
+garage.Add(ashleysCar);
+garage.Add(gavinsCar);
+
+//showing we can access those cars and their properties from our list
+Console.WriteLine(garage[1].Model);
